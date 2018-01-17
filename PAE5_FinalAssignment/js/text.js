54988a95b3a7045206158d28d26dd20bae43878e
Nabe0416 <34216014+Nabe0416@users.noreply.github.com>
2018-01-17 23:47:38 +0100

    Codes have been moved into screen.js

--- PAE5_FinalAssignment/js/text.js
+++
@@ -1,4 +0,0 @@
-var mainScreenHeader = "Chemistry Quiz";
-var mainScreenDescription = "In this quiz you will randomly get 10 questions about chemistry in your daily life. <br/>"+ 
-"Try to get it right as many as possible! <br/>" +
-"You can get the right answers after the quiz, so you will learn something that you are not familiar with.";
